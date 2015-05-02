5efa12af1cb76304d8cd85183a7008f6c651407f
netsirkdmb <kristendhuse+github@gmail.com>
2015-05-02 08:25:50 -0700

    Finished multiplication table for multtable.php.

--- assignment4-part1/src/multtable.php
+++ assignment4-part1/src/multtable.php
@@ -3,6 +3,18 @@
 <head>
 <meta charset="UTF-8">
 <title>Multtable</title>
+<style>
+table{
+  border: thin solid black;
+  border-collapse: collapse;
+
+}
+th, td{
+  border: thin solid black;
+  text-align: center;
+  padding: 5px;
+}
+</style>
 </head>
 <body>
 <?php
@@ -16,13 +28,13 @@
   $isError = false;
   foreach ($names as $name) {
     if (!isset($_GET[$name])) {
-      echo "Missing parameter " . $name . ".<br>";
+      echo 'Missing parameter ' . $name . '.<br>';
       $isError = true;
       continue;
     }
     $value = $_GET[$name];
     if (!is_numeric($value) || intval($value) != floatval($value) || intval($value) < 0) {
-      echo $name . " must be an integer.<br>";
+      echo $name . ' must be an integer.<br>';
       $isError = true;
     }
   }
@@ -34,17 +46,31 @@
   $min_multiplier = $_GET["min-multiplier"];
   $max_multiplier = $_GET["max-multiplier"];
   if ($min_multiplicand > $max_multiplicand) {
-    echo "Minimum multiplicand larger than maximum.<br>";
+    echo 'Minimum multiplicand larger than maximum.<br>';
     $isError = true;
   }
   if ($min_multiplier > $max_multiplier) {
-    echo "Minimum multiplier larger than maximum.<br>";
+    echo 'Minimum multiplier larger than maximum.<br>';
     $isError = true;
   }
   if ($isError) {
     exit;
   }
-  
+  echo '<caption>Multiplication Table</caption>
+        <table>
+        <tr>
+        <td>';
+  for($i = $min_multiplier; $i <= $max_multiplier; $i++) {
+    echo '<th>' . $i;
+  }
+  for($i = $min_multiplicand; $i <= $max_multiplicand; $i++) {
+    echo '<tr>
+          <th>' . $i;
+    for($j = $min_multiplier; $j <= $max_multiplier; $j++) {
+      echo '<td>' . $i * $j;
+    }
+  }
 ?>
+
 </body>
 </html>
